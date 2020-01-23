d369369d135f1888f29c5eae35e266769ba73a04
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-23 07:37:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+
+function map(val, cd) {
+let v = []
+
+for (let i = 0; i < val.length; i++) {
+  let theElement = val[i]
+  v.push(cd(theElement))
+}
+
+return r;
+}
+
+function reduce(val, cd, starting){
+let v = (!!starting) ? starting : src[0]
+let i = (!!starting) ? 0 : 1
+
+for (let i = 0; i < val.length; i++) {
+  v = cd(val[i], v)
+}
+
+return v;
+}
